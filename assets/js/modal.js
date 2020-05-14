fb7f657ecb8c5f22f6bc415febbfae86ca174663
Elisabeth Farias <elisabethfarias7@gmail.com>
2020-05-14 13:52:16 -0300

    Created pop-up

---
+++ assets/js/modal.js
@@ -0,0 +1,25 @@
+function showModal(modalID) {
+  const modal = document.getElementById(modalID)
+  if(modal) {
+    modal.classList.add('show')
+    modal.addEventListener('click', (e) => {
+      if(e.target.className == 'close-modal' || e.target.id == modalID) {
+        modal.classList.remove('show')
+      }
+    })
+  }
+  
+}
+
+function onMouseOut(event) {
+  // If the mouse is near the top of the window, show the popup
+  if (event.clientY < 50) {
+    // Remove this event listener
+    document.removeEventListener("mouseout", onMouseOut);
+
+    // Show the popup
+    showModal('exit-intent-modal')
+  }
+}
+
+document.addEventListener("mouseout", onMouseOut);
