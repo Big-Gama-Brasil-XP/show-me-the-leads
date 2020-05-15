49ea0ec3ba45ddc876daeb1c742fbfbd6e554ac0
pmoreira <pmoreira@firenzze.com>
2020-05-14 22:10:19 -0300

    fix exit intent modal button behavior and simplify the html and css code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,16 +1,35 @@
-function showModal(modalID) {
+function showModal (modalID) {
+
   const modal = document.getElementById(modalID)
-  if(modal) {
-    modal.classList.add('show')
+
+  if (modal) {
+   
+    modal.classList.add('o-exit-modal--visible')
+    
     modal.addEventListener('click', (e) => {
-      if(e.target.className == 'close-modal' || e.target.id == modalID) {
-        modal.classList.remove('show')
+
+      const targetId = e.target.id
+
+      if (targetId == 'button-close-modal' || 
+          targetId == 'button-thanks' || 
+          targetId == 'button-answer' || 
+          targetId == modalID) {
+
+        modal.classList.remove('o-exit-modal--visible')
+
+      } 
+      
+      if (targetId == 'button-answer') {
+        window.scrollTo(0, 0)
       }
+
     })
+
   }
+
 }
 
-function onMouseOut(event) {
+function onMouseOut (event) {
   // If the mouse is near the top of the window, show the popup
   if (event.clientY < 50) {
     // Remove this event listener
@@ -22,12 +41,3 @@ function onMouseOut(event) {
 }
 
 document.addEventListener("mouseout", onMouseOut);
-
-
-function toggle(obj1) {
-  var el = document.getElementById(obj1);
-
-  if ( el.style.display != 'none' ) {
-  el.style.display = 'none';
-  }
-}
